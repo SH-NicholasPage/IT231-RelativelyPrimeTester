e224f881bea15210fb2e3a873fc1806acfb84d16
SH-NicholasPage <npage@southhills.edu>
2022-08-30 22:04:02 -0400

    Initial commit

---
+++ RelativelyPrimeTester/RelativelyPrimeTester/Source.cs
@@ -0,0 +1,37 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RelativelyPrimeTester.Program;
+
+namespace RelativelyPrimeTester
+{
+    public class Source
+    {
+        public static void Tester()
+        {
+            ProcessResult pr1 = Utils.RunProcess(0, 2);
+            ProcessResult pr2 = Utils.RunProcess("1");
+            ProcessResult pr3 = Utils.RunProcess("1 s");
+            ProcessResult pr4 = Utils.RunProcess("s");
+            ProcessResult pr5 = Utils.RunProcess("");
+            ProcessResult pr6 = Utils.RunProcess("12 12");
+            ProcessResult pr7 = Utils.RunProcess("12 12 5");
+            ProcessResult pr8 = Utils.RunProcess("99999999999999999999999999 12");
+
+            int t = 0;
+        }
+
+        public static void ExhaustiveTest()
+        {
+            
+        }
+    }
+}
---
+++ RelativelyPrimeTester/RelativelyPrimeTester/Utils.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * You can add other methods to this file if you would like, but do not modify existing methods.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RelativelyPrimeTester.Program;
+
+namespace RelativelyPrimeTester
+{
+    public class Utils
+    {
+        //IO is VERY slow, so it's worth muting after enough runs, especially for the exhaustive check
+        private static readonly int MUTE_OUTPUT_AFTER_X_RUNS = 20;
+        private static int processRan = 0;//Increases every time RunProcess is called
+
+        public static ProcessResult RunProcess()
+        {
+            return RunProcess(String.Empty, String.Empty);
+        }
+
+        public static ProcessResult RunProcess(int? num1 = null, int? num2 = null)
+        {
+            return RunProcess(num1.ToString(), num2.ToString());
+        }
+
+        public static ProcessResult RunProcess(String? val1 = null, String? val2 = null)
+        {
+            int num1 = int.MinValue;
+            int num2 = int.MinValue;
+
+            int.TryParse(val1, out num1);
+            int.TryParse(val2, out num2);
+
+            if (num1 != int.MinValue && num2 != int.MinValue)
+            {
+                Program.NumChecked(num1, num2);
+            }
+
+            bool mute = false;
+
+            if (processRan > MUTE_OUTPUT_AFTER_X_RUNS)
+            {
+                mute = true;
+            }
+            else
+            {
+                processRan++;
+            }
+
+            Process process = new Process();
+            process.StartInfo.FileName = "RelativelyPrime/RelativelyPrime.exe";
+            process.StartInfo.Arguments = val1 + " " + val2;
+            process.Start();
+            process.WaitForExit();
+
+            if (process.ExitCode < 0)
+            {
+                Program.RecieveResult(process.ExitCode);
+                Console.WriteLine("Relatively Prime application crashed!");
+                return ProcessResult.Crashed;
+            }
+
+            if (mute == false)
+            {
+                Console.WriteLine("Relatively Prime application executed without issue. Comnination was reported as " + ((process.ExitCode == 1) ? " coprime." : "not coprime."));
+            }
+
+            return (ProcessResult)process.ExitCode;
+        }
+
+        public static bool IsCoPrime(int val1, int val2)
+        {
+            return (GCD(val1, val2) == 1) ? true : false;
+        }
+
+        //Recursive
+        private static int GCD(int a, int b)
+        {
+            //Everything divides 0
+            if (a == 0 || b == 0)
+            {
+                return 0;
+            }
+
+            //Base case
+            if (a == b)
+            {
+                return a;
+            }
+
+            //A is greater
+            if (a > b)
+            {
+                return GCD(a - b, b);
+            }
+
+            return GCD(a, b - a);
+        }
+    }
+}
