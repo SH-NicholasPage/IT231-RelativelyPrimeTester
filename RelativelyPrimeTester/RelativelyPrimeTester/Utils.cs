4995f7bd2b79008a11c616df60ca05000b58b24f
wheat32 <npage@southhills.edu>
2022-09-14 08:42:22 -0400

    Mac changes

--- RelativelyPrimeTester/RelativelyPrimeTester/Utils.cs
+++ RelativelyPrimeTester/RelativelyPrimeTester/Utils.cs
@@ -60,6 +60,7 @@ namespace RelativelyPrimeTester
             else if(OperatingSystem.IsMacOS())
             {
                 process.StartInfo.FileName = "RelativelyPrime/macOS/RelativelyPrime.app";
+                process.StartInfo.UseShellExecute = true;
             }
             process.StartInfo.Arguments = val1 + " " + val2;
             process.Start();
