6ca8448b58246b978566ca4cb78b96b0faa545ee
Nicholas Page <110427598+SH-NicholasPage@users.noreply.github.com>
2025-02-20 12:08:25 -0500

    Update Source.cs

--- RelativelyPrimeTester/Source.cs
+++ RelativelyPrimeTester/Source.cs
@@ -12,31 +12,32 @@ using System.Threading;
 using System.Threading.Tasks;
 using static RelativelyPrimeTester.Program;
 
-namespace RelativelyPrimeTester
+namespace RelativelyPrimeTester;
+
+// If using Linux, navigate to RealtvielyPrime/Linux and give the exe file execute permission.
+//    If you've already built, do a clean build.
+
+public class Source
 {
-    public class Source
+    public static void Tester()
     {
-        public static void Tester()
-        {
-            // TODO: Edit the code here to test the Relatively Prime program for errors.
-            //  There are six different errors you can find, but you only need to find five.
+        // TODO: Edit the code here to test the Relatively Prime program for errors.
+        //  There are six different errors you can find, but you only need to find five.
 
-            Result example1 = Process.Run(24, 32);
-            Result example2 = Process.Run("24 32");//In this format, numbers must be space separated.
+        Result example1 = Process.Run(24, 32);
+        Result example2 = Process.Run("24 32");//In this format, numbers must be space separated.
 
-            // Hint: Use breakpoints to check the values of variables during runtime and keep checking the console.
-        }
+        // Hint: Use breakpoints to check the values of variables during runtime and keep checking the console.
+    }
 
-        public static void ExhaustiveTest()
-        {
-            int logicErrors = 0;
-            
-            Thread.Sleep(1200);
+    public static void ExhaustiveTest()
+    {
+        int logicErrors = 0;
 
-            // TODO: Write your exhaustive test here.
-            // Don't worry about timing it, there are functions in place that will time it for you.
+        // TODO: Write your exhaustive test here.
+        // Don't worry about timing it, there are functions in place that will time it for you.
 
-            Console.WriteLine("Logic errors found: " + logicErrors);
-        }
+        Console.WriteLine("Logic errors found: " + logicErrors);
     }
 }
+
